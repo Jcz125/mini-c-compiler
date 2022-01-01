6daba88aa29bc9ba89c3ea47c26a9248d67618af
Chaima Tounsi Omezzine <chaima.tounsi-omezzine@telecomnancy.eu>
2022-01-01 19:37:38 +0100

    ajout de fct de recherche dans la TDS

--- src/TDS/SymbolTable.java
+++ src/TDS/SymbolTable.java
@@ -18,6 +18,7 @@ public class SymbolTable {
         this.children = new ArrayList<>() ;
     }
 
+
     public LineElement addLineInt(String idf, NatureSymboles nature) {
         for (LineElement line:lines) {
             if (line.getIdf().equals(idf)) {
@@ -30,6 +31,7 @@ public class SymbolTable {
         lines.add(newLine) ;
         return newLine;
     }
+
 
     public LineElement addLineStruct(String idf, NatureSymboles nature, String type) {
         for (LineElement line:lines) {
@@ -44,6 +46,7 @@ public class SymbolTable {
         return newLine;
     }
 
+
     public LineElement addLineFct(String idf, NatureSymboles nature, String typeRetour, HashMap<String, Symbole> fctParams, int nbParams) {
         for (LineElement line:lines) {
             if (line.getIdf().equals(idf)) {
@@ -56,6 +59,7 @@ public class SymbolTable {
         lines.add(newLine) ;
         return newLine;
     }
+
 
     public LineElement addLineStructDef(String idf, NatureSymboles nature, String type, HashMap<String, Symbole> champs) {
         for (LineElement line:lines) {
@@ -71,7 +75,25 @@ public class SymbolTable {
     }
 
 
-//
+    public LineElement search(String idf, SymbolTable st) {
+        for (LineElement line:lines) {
+            if (line.getIdf().equals(idf)) {
+                System.out.println("Var declared ");
+                return line;
+            }
+        }
+        System.out.println("Not found in the current table, searching in parent : ");
+
+        if (st.parent != null)
+            return search(idf, st.parent);
+
+        return null ;
+    }
+
+
+
+
+
 //    public LineElement updateLineInt(String idf, String value) {
 //        for (LineElement line:lines) {
 //            if (line.getIdf().equals(idf) && (line.getValue().equals(""))) {
