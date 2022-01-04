d0459146882dd5bf708a39dba29753c3d9535931
Chaima Tounsi Omezzine <chaima.tounsi-omezzine@telecomnancy.eu>
2022-01-04 12:04:01 +0100

    ajout des tests while

--- src/TDS/test.c
+++ src/TDS/test.c
@@ -1,8 +1,11 @@
+#include <stdlib.h>
+
 int fct(int a, int b, int c, int d, int e, int f){
     if (a<b>c==d<=f) return 1 ;
     else if (a>=b>=c>d>=e>f>=10) return 2 ;
     else if (a==b==d==10) return 3 ;
     else if (a<=b<c<=d<e<=f<10) return 4 ;
+    else if (a=b=c=d=e=f=10) return 5 ;
     else if (a<b>c<d>e<f>10) return 6 ;
     else if (a>b<c>d<e>f<10) return 7 ;
     else return 0;
